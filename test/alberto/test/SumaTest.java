f5548aed78bdaf513c7713a25e5997e09edb5df2
7Alberto7 <j.albertocg86@gmail.com>
2013-10-25 20:13:27 +0200

    Init

---
+++ test/alberto/test/SumaTest.java
@@ -0,0 +1,31 @@
+package alberto.test;
+
+import static org.junit.Assert.*;
+
+import org.junit.*;
+
+import alberto.Suma;
+
+public class SumaTest {
+	private Suma suma;
+
+	@Before
+	public void paraEjecutarAntes() {
+		suma = new Suma();
+	}
+
+	@Test
+	public void aVerSiIncrementaBien() {
+		assertEquals("Test incrementa", 2.0, suma.incrementa(1.0), 1e-6);
+	}
+
+	@Test
+	public void aVerSiSumaBien() {
+		assertEquals("Test suma", 2.0, suma.getSuma(1.0, 1.0), 1e-6);
+	}
+
+	@After
+	public void paraEjecutarDespues() {
+		// Fin de test. Aqui liberar recursos o borrar rastros del test
+	}
+}
